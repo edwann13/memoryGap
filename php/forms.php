c4fb9ec57333e25f07d43f2e1de15bc1da255be7
edwann13 <edwanhernandez@gmail.com>
2016-04-25 18:49:09 -0700

    Created the php file that will hold the forms data

---
+++ php/forms.php
@@ -0,0 +1,10 @@
+<?php
+/** @author: Edwan Hernandez 4/25/2016
+  * Will generate the forms that will allow the
+  * user to input their class and what topic or
+  * chapter(or subchapter) they want to implement
+  * spaced repetition. */
+	echo '<h1>This is the form, made with php.</h1>';
+
+
+?>
